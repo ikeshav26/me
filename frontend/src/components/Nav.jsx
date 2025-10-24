dc85fbad436d1da66940ba5eec70a7e4974a213a
ikeshav26 <keshavgilhotra4@gmail.com>
2025-10-25 03:33:26 +0530

    Home page added

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,17 +7,19 @@ const Nav = () => {
   const [dropdown, setdropdown] = useState(false);
   
   const navItems = [
-    { name: "Home", path: "/#home" },
-    { name: "About", path: "/#about" },
-    { name: "Projects", path: "/#projects" },
-    { name: "Contact", path: "/#contact" },
+    { name: "Homes", path: "/#home" ,color:"#EAB308"},
+    { name: "About", path: "/#about" ,color:"#3B82F6"},
+    { name: "Projects", path: "/#projects" ,color:"#14bA86"},
+    { name: "Contact", path: "/#contact",color:"#6366f1" },
   ];
 
-  // useGSAP(()=>{
-  //   gsap.fromTo('.dropdown',{
-  //     borderRadius:"50%"
-  //   })
-  // })
+  const socialLinks=[
+    {name:"Github",path:"https://github.com/ikeshav26"},
+    {name:"LinkedIn",path:"https://www/linkedin.com/in/keshav-gilhotra"},
+    {name:"Twitter",path:"https://twitter.com/keshavgilh95"},
+    {name:"Instagram",path:"https://www.instagram.com/keshav_gilhotra_"},
+  ]
+
 
 
   return (
@@ -42,22 +44,48 @@ const Nav = () => {
 
      
       <div
-        className={`dropdown fixed top-0 right-0 w-full sm:w-1/3 h-screen bg-[#1a1a1a] z-40 transition-transform duration-500 ease-in-out ${
+        className={`dropdown fixed top-0 right-0 w-full sm:w-1/3 flex h-screen bg-[#1a1a1a] z-40 transition-transform duration-500 ease-in-out ${
           dropdown ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <nav className="flex flex-col items-center justify-center h-full gap-8">
-          {navItems.map((item, index) => (
+        <nav className="flex flex-col items-center justify-center h-full w-1/2 gap-8 ">
+        <h1 className="font-[font2] text-2xl text-[#c8c8c8]/60 mb-6">SOCIAL</h1>
+          {socialLinks.map((item, index) => (<>
             <Link
               key={index}
               to={item.path}
-              className="text-4xl font-bold text-[#c8c8c8] hover:text-white transition-colors duration-300 cursor-pointer"
+              className="text-xl font-[font2]  text-[#c8c8c8] hover:text-white transition-colors duration-300 cursor-pointer"
             >
               {item.name}
             </Link>
+            </>
           ))}
         </nav>
+        <nav className="flex flex-col items-center justify-center h-full w-1/2 gap-8 ">
+        <h1 className="font-[font2] text-2xl text-[#c8c8c8]/60 mb-6">MENU</h1>
+          {navItems.map((item, index) => (<>
+            <Link
+              key={index}
+              to={item.path}
+              className="text-xl font-[font2] flex  items-center gap-2  text-[#c8c8c8] hover:text-white transition-colors duration-300 cursor-pointer"
+            >
+              <div className="w-2 h-2 rounded-full" style={{ backgroundColor: item.color }}></div>{item.name}
+            </Link>
+            </>
+          ))}
+        </nav>
+
+        <div className={`fixed bottom-15 left-1/6 font-[font2] flex flex-col gap-4 text-xl z-50 transition-opacity duration-500 ${
+        dropdown ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}>
+        <div className="text-[#c8c8c8]/60">GET IN TOUCH</div>
+        <div className="text-[#c8c8c8]">keshavgilhotra4@gmail.com</div>
       </div>
+      </div>
+
+
+      <div className='fixed -left-21 top-3/4 font-[font2] text-[#c8c8c8]/70 rotate-90 text-md'>keshavgilhotra4@gmail.com</div>
+
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen '>
-      Home
+    <div className='h-screen relative px-6 md:px-0'>
+      <div className='absolute top-1/4 md:top-1/4 left-6 md:left-1/4 w-[calc(100%-3rem)] md:w-1/3 h-auto md:h-90 flex flex-col justify-between gap-8 md:gap-0'>
+
+      <div className='flex flex-col text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-[font1]'>
+        <span className='text-[#00f050]'>FULL STACK</span>
+        <span className='ml-4 md:ml-8'>DEVELOPER</span>
+      </div>
+
+      <div className='text-base sm:text-lg md:text-xl text-[#c8c8c8]/60'>
+        Hi! I'm <span className='text-[#c8c8c8]'>Keshav Gilhotra</span>. A Full Stack Developer with experience of building scalable, high-performance web apps. Currently enhancing my skills in Java and DSA to strengthen my backend and problem-solving expertise.
+      </div>
+
+      <div>
+        <button className='bg-[#00f050] text-black font-[font1] text-lg md:text-xl py-2 px-4 hover:bg-[#00d045] transition-colors duration-300'>Download CV</button>
+      </div>
+      </div>
+
+      <div className='h-auto md:h-90 w-auto md:w-50 absolute right-6 md:right-16 bottom-12 md:bottom-1/5 flex flex-col justify-between items-end gap-6 md:gap-0'>
+      <div className='flex flex-col items-end'>
+        <div className='text-[#00f050] text-2xl sm:text-3xl font-[font1]'>1+</div>
+        <div className='text-[#c8c8c8]/60 text-sm sm:text-base md:text-lg font-[font2]'>Years of Experience</div>
+      </div>
+      <div className='flex flex-col items-end'>
+        <div className='text-[#00f050] text-2xl sm:text-3xl font-[font1]'>5+</div>
+        <div className='text-[#c8c8c8]/60 text-sm sm:text-base md:text-lg font-[font2]'>Completed Projects</div>
+      </div>
+      <div className='flex flex-col items-end'>
+        <div className='text-[#00f050] text-2xl sm:text-3xl font-[font1]'>2.5K+</div>
+        <div className='text-[#c8c8c8]/60 text-sm sm:text-base md:text-lg font-[font2]'>Hours Worked</div>
+        </div>
+        </div>
     </div>
   )
 }
