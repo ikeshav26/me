223233be60f58230107e050cf83dbecfb42ad28e
ikeshav26 <keshavgilhotra4@gmail.com>
2025-11-22 03:50:00 +0530

    Analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import LandingPage from './components/LandingPage.jsx'
 import Background from './components/Background.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
     <LandingPage>
       <Background>
