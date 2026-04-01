475ad7988725113b0734435faf8c8098f6e88bc5
ikeshav26 <keshavgilhotra4@gmail.com>
2026-04-02 00:29:12 +0530

    Changes for addition of server

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express, { type Request, type Response } from 'express';
+
+const app: express.Application = express();
+
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('Portfolio Backend is running');
+})
+
+
+export default app;
