6b69b7f5f1371aab45b014a1308603aeeab2e57b
ikeshav26 <keshavgilhotra4@gmail.com>
2026-05-28 00:02:15 +0530

    Some changes in landing animation

--- client/src/hooks/Stairs.tsx
+++ client/src/hooks/Stairs.tsx
@@ -7,44 +7,56 @@ const LandingPage = ({children}: {children: React.ReactNode}) => {
     const {theme}=useTheme()
 
     useGSAP(()=>{
-      const tl=gsap.timeline()
-      const tl2=gsap.timeline()
+      const tl = gsap.timeline()
 
-      tl.from('.name',{
-          opacity:0,
-          y:100,
-          stagger:0.05,
-          duration: 0.5,
-          ease: 'power2.out'
+      tl.from('.name', {
+          opacity: 0,
+          y: 100,
+          scale: 0.8,
+          stagger: {
+            each: 0.05,
+            from: "center" 
+          },
+          duration: 0.8,
+          ease: 'expo.out'
       })
 
-        tl2.to('.stair',{
-            yPercent:100,
-            stagger:0.05,
-            duration: 0.4,
-            delay:0.8
-        })
 
-        tl.to('.name',{
-           opacity:0,
-           y:-50,
-           stagger:0.05,
-           duration: 0.3,
-           delay:0.5,
-           ease: 'power2.in'
-        })
-        
-        tl2.to('.main',{
+      tl.to('.name', {
            opacity: 0,
-           duration: 0.2,
-           display: 'none'
-        })
+           y: -50,
+           scale: 0.9,
+           stagger: {
+             each: 0.04,
+             from: "center"
+           },
+           duration: 0.4,
+           ease: 'power2.in'
+      }, "+=0.4") 
+        
+
+      tl.to('.stair', {
+          yPercent: 100,
+          stagger: {
+            each: 0.05,
+            from: "start" 
+          },
+          duration: 0.7,
+          ease: 'power4.inOut'
+      }, "<0.2") 
+
+
+      tl.to('.main', {
+         opacity: 0,
+         duration: 0.1,
+         display: 'none'
+      })
     })
 
   return (
     <>
     <div className='main fixed inset-0 z-100 flex overflow-hidden'>
-      <div className={theme === 'light' ? 'group z-60 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[15vw] font-bold text-black' : 'group z-60 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[15vw] font-bold text-white'}>
+      <div className={theme === 'dark' ? 'group z-60 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[15vw] font-bold text-black' : 'group z-60 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[15vw] font-bold text-white'}>
       <span className='name'>K</span>
       <span className='name'>E</span>
       <span className='name'>S</span>
@@ -52,16 +64,16 @@ const LandingPage = ({children}: {children: React.ReactNode}) => {
       <span className='name'>A</span>
       <span className='name'>V</span>
       </div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
-      <div className={`stair h-screen w-[10vw] ${theme === 'light' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
+      <div className={`stair h-screen w-[10vw] ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}></div>
     </div>
     <div>
         {children}
